8ae2dfe5a80d734c2f3a38a55925924b24a6d63e
shaonbishwas <shaonbishwas01@gmail.com>
2023-12-06 16:49:55 +0600

    checking

--- forum/src/Layouts/Login/Login.jsx
+++ forum/src/Layouts/Login/Login.jsx
@@ -1,7 +1,8 @@
 const Login = () => {
   return (
-    <div>
-      <div className="card w-full max-w-sm bg-base-100">
+    <div className="flex items-center justify-center h-screen">
+      <div className="card w-full max-w-sm bg-base-100 shadow ">
+        <h1 className="text-3xl font-semibold text-center">Login</h1>
         <form className="card-body">
           <div className="form-control">
             <label className="label">
@@ -31,7 +32,7 @@ const Login = () => {
             </label>
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary bg-[#E58B00] border-none text-white">Login</button>
+            <button className="btn bg-[#B68C5A]  text-white hover:bg-white hover:border hover:border-[#B68C5A] hover:text-black">Login</button>
           </div>
         </form>
       </div>
